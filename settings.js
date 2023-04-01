2496222127de90793df76c1a18384a55d7efd2a7
MTKA08312009 <128493475+MTKA08312009@users.noreply.github.com>
2023-04-01 19:12:11 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,7 +18,7 @@ global.APIs = {
 //buy apikey premium 0887435047326
 // Free apikey (silahkan login terus ganti V12 dgn apikey lu)
 global.APIKeys = {
-   'https://api.zeeoneofc.my.id': 'V12', // 👉 login https://api.zeeoneofc.my.id to get apikey
+   'https://api.zeeoneofc.my.id': 'wgdLq7D92zmnbb2', // 👉 login https://api.zeeoneofc.my.id to get apikey
 }
 global.sewabotnya = `
 edit sewabot di file settings.js line 24
@@ -46,18 +46,18 @@ global.language = indonesia //change indonesia to english if you don't understan
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/@ZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://s.id/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "pacarnya sakip 7e" //namabot kalian
+global.ownername = "Tsaqif 7e" //nama kalian
+global.myweb = "https://replit.com/@Kask62id" //bebas asal jan hapus
+global.youtube = "https://www.youtube.com/channel/UCJEL4sy7RRCbiuXiW07KM-A" //bebas asal jan hapus
+global.myweb2 = "-" //bebas
+global.email = "tsaqifkhasyi@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "62821931157232"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+"+ownernomer //nmr wa kalian
+global.premium = ['6281393052861'] //premium user
+global.owner = ["6281393052861", "6281393052861"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6281393052861" // nomor wa kalian
+global.ownernomerr = "+6281393052861"+ownernomer //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/edca4b513ddf29fd52c0e.jpg" // maks size 30kb, agar welcome image nya tdk delay
